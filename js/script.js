85fd6a4128899cb205a9a635676420d8a9c6facc
haley-m <124923973+haley-m@users.noreply.github.com>
2024-07-05 20:30:11 +0900

    BP-plc
    BP plc - Responsive Website

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+  // <!-- Initialize Swiper for Section_2-->
+
+    var swiper = new Swiper(".mySwiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
+    // <!-- Initialize Swiper for Sectoin_3 -->
+      var swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+        },
+      });
+      
